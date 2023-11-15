e487d8acb8d71f138c27179b02224a9f27ccb5bd
David Pedersen <david.pdrsn@gmail.com>
2023-11-15 18:57:31 +0100

    Update dependencies
    Updates to 1.0 of hyper, http, and friends.

--- src/server/conn/auto.rs
+++ src/server/conn/auto.rs
@@ -13,7 +13,7 @@ use http::{Request, Response};
 use http_body::Body;
 use hyper::{
     body::Incoming,
-    rt::{bounds::Http2ConnExec, Timer},
+    rt::{bounds::Http2ServerConnExec, Timer},
     server::conn::{http1, http2},
     service::Service,
 };
@@ -75,7 +75,7 @@ impl<E> Builder<E> {
         B::Data: Send,
         B::Error: Into<Box<dyn StdError + Send + Sync>>,
         I: AsyncRead + AsyncWrite + Unpin + 'static,
-        E: Http2ConnExec<S::Future, B>,
+        E: Http2ServerConnExec<S::Future, B>,
     {
         let (version, io) = read_version(io).await?;
         let io = TokioIo::new(io);
@@ -99,7 +99,7 @@ impl<E> Builder<E> {
         B::Data: Send,
         B::Error: Into<Box<dyn StdError + Send + Sync>>,
         I: AsyncRead + AsyncWrite + Unpin + Send + 'static,
-        E: Http2ConnExec<S::Future, B>,
+        E: Http2ServerConnExec<S::Future, B>,
     {
         let (version, io) = read_version(io).await?;
         let io = TokioIo::new(io);
@@ -303,7 +303,7 @@ impl<E> Http1Builder<'_, E> {
         B::Data: Send,
         B::Error: Into<Box<dyn StdError + Send + Sync>>,
         I: AsyncRead + AsyncWrite + Unpin + 'static,
-        E: Http2ConnExec<S::Future, B>,
+        E: Http2ServerConnExec<S::Future, B>,
     {
         self.inner.serve_connection(io, service).await
     }
@@ -451,7 +451,7 @@ impl<E> Http2Builder<'_, E> {
         B::Data: Send,
         B::Error: Into<Box<dyn StdError + Send + Sync>>,
         I: AsyncRead + AsyncWrite + Unpin + 'static,
-        E: Http2ConnExec<S::Future, B>,
+        E: Http2ServerConnExec<S::Future, B>,
     {
         self.inner.serve_connection(io, service).await
     }
