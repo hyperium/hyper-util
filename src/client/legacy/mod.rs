a89ea052a0a4b00abeb12aa38e622b9bd07c0e94
John Howard <howardjohn@google.com>
2023-11-16 09:31:36 -0800

    Re-expose connect::Error after #49 (#51)
    Since #49, we cannot refer to the Error response from the client. This
    exposes it again

--- src/client/legacy/mod.rs
+++ src/client/legacy/mod.rs
@@ -2,5 +2,7 @@
 mod client;
 #[cfg(any(feature = "http1", feature = "http2"))]
 pub use client::Client;
+#[cfg(any(feature = "http1", feature = "http2"))]
+pub use client::Error;
 
 pub mod connect;
