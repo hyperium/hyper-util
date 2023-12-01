bca85f782007de1cc72a4b0365cdfa1ef28afd5e
Kushagra Udai <112903599+kushudai@users.noreply.github.com>
2023-12-01 05:22:31 -0800

    feat: export client::legacy::Builder (#69)

--- src/client/legacy/mod.rs
+++ src/client/legacy/mod.rs
@@ -1,9 +1,7 @@
 #[cfg(any(feature = "http1", feature = "http2"))]
 mod client;
 #[cfg(any(feature = "http1", feature = "http2"))]
-pub use client::Client;
-#[cfg(any(feature = "http1", feature = "http2"))]
-pub use client::Error;
+pub use client::{Builder, Client, Error};
 
 pub mod connect;
 #[doc(hidden)]
