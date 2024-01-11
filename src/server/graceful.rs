c78a8eb917642fd278b2544e32361938aa3e2faf
glendc <glen.decauwsemaecker@otainsight.com>
2024-01-11 10:11:49 +0100

    implement gracefull connection support
    in replacement of futures in generic

--- src/server/graceful.rs
+++ src/server/graceful.rs
@@ -7,18 +7,20 @@
 //!
 //! TODO
 
+use futures_util::FutureExt;
 use pin_project_lite::pin_project;
 use slab::Slab;
 use std::{
     fmt::{self, Debug, Display},
     future::Future,
-    pin::Pin,
+    pin::{pin, Pin},
     sync::{
         atomic::{AtomicBool, AtomicUsize},
         Arc, Mutex,
     },
     task::{self, Poll, Waker},
 };
+use tokio::select;
 
 /// A graceful shutdown watcher
 #[derive(Clone)]
@@ -52,19 +54,40 @@ impl GracefulShutdown {
 
     /// Watch a future for graceful shutdown,
     /// returning a wrapper that can be awaited on.
-    pub fn watch<F, T>(&self, f: F) -> Result<GracefulFuture<F>, GracefulShutdownError<F>>
+    pub fn watch<C, F>(
+        &self,
+        conn: C,
+        cancel: F,
+    ) -> Result<GracefulFuture<C::Error>, GracefulShutdownError<C>>
     where
-        F: Future<Output = T>,
+        C: GracefulConnection + Send + Sync + 'static,
+        F: Future + Send + Sync + 'static,
     {
         if self
             .state
             .shutdown
             .load(std::sync::atomic::Ordering::SeqCst)
         {
-            return Err(GracefulShutdownError { future: f });
+            return Err(GracefulShutdownError { conn });
         }
         Ok(GracefulFuture {
-            future: f,
+            future: Box::pin(async move {
+                let mut conn = pin!(conn);
+                let mut cancel = pin!(cancel.fuse());
+
+                loop {
+                    select! {
+                        _ = cancel.as_mut() => {
+                            tracing::trace!("signal received: initiate graceful shutdown");
+                            conn.as_mut().graceful_shutdown();
+                        }
+                        result = conn.as_mut() => {
+                            tracing::trace!("connection finished");
+                            return result;
+                        }
+                    }
+                }
+            }),
             state: Some(self.state.clone()),
         })
     }
@@ -124,9 +147,9 @@ pin_project! {
     ///
     /// Whether or not this future panics in such cases is an implementation detail
     /// and should not be relied upon.
-    pub struct GracefulFuture<F> {
+    pub struct GracefulFuture<E> {
         #[pin]
-        future: F,
+        future: BoxFuture<E>,
         state: Option<Arc<GracefulState>>,
     }
 }
@@ -137,11 +160,8 @@ impl<F> Debug for GracefulFuture<F> {
     }
 }
 
-impl<F> Future for GracefulFuture<F>
-where
-    F: Future,
-{
-    type Output = F::Output;
+impl<E> Future for GracefulFuture<E> {
+    type Output = Result<(), E>;
 
     fn poll(self: Pin<&mut Self>, cx: &mut task::Context<'_>) -> Poll<Self::Output> {
         let this = self.project();
@@ -215,27 +235,184 @@ struct GracefulWaiterState {
 
 /// The error type returned by [`GracefulShutdown::watch`],
 /// when a graceful shutdown is already in progress.
-pub struct GracefulShutdownError<F> {
-    future: F,
+pub struct GracefulShutdownError<C> {
+    conn: C,
 }
 
-impl<F> GracefulShutdownError<F> {
-    /// Get back the future that errored with a [`GracefulShutdownError`].
-    pub fn into_future(self) -> F {
-        self.future
+impl<C> GracefulShutdownError<C> {
+    /// Get back the connection that errored with a [`GracefulShutdownError`].
+    pub fn into_connection(self) -> C {
+        self.conn
     }
 }
 
-impl<F> Display for GracefulShutdownError<F> {
+impl<C> Display for GracefulShutdownError<C> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.write_str("graceful shutdown already in progress")
     }
 }
 
-impl<F> Debug for GracefulShutdownError<F> {
+impl<C> Debug for GracefulShutdownError<C> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         Display::fmt(self, f)
     }
 }
 
 impl<F> std::error::Error for GracefulShutdownError<F> {}
+
+type BoxFuture<E> = Pin<Box<dyn Future<Output = Result<(), E>> + Send + Sync + 'static>>;
+
+/// An internal utility trait as an umbrella target for all (hyper) connection
+/// types that the [`GracefulShutdown`] can watch.
+pub trait GracefulConnection: Future<Output = Result<(), Self::Error>> + private::Sealed {
+    /// The error type returned by the connection when used as a future.
+    type Error;
+
+    /// Start a graceful shutdown process for this connection.
+    fn graceful_shutdown(self: Pin<&mut Self>);
+}
+
+#[cfg(feature = "http1")]
+impl<I, B, S> GracefulConnection for hyper::server::conn::http1::Connection<I, S>
+where
+    S: hyper::service::HttpService<hyper::body::Incoming, ResBody = B>,
+    S::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+    I: hyper::rt::Read + hyper::rt::Write + Unpin + 'static,
+    B: hyper::body::Body + 'static,
+    B::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+{
+    type Error = hyper::Error;
+
+    fn graceful_shutdown(self: Pin<&mut Self>) {
+        hyper::server::conn::http1::Connection::graceful_shutdown(self);
+    }
+}
+
+#[cfg(feature = "http2")]
+impl<I, B, S, E> GracefulConnection for hyper::server::conn::http2::Connection<I, S, E>
+where
+    S: hyper::service::HttpService<hyper::body::Incoming, ResBody = B>,
+    S::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+    I: hyper::rt::Read + hyper::rt::Write + Unpin + 'static,
+    B: hyper::body::Body + 'static,
+    B::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+    E: hyper::rt::bounds::Http2ServerConnExec<S::Future, B>,
+{
+    type Error = hyper::Error;
+
+    fn graceful_shutdown(self: Pin<&mut Self>) {
+        hyper::server::conn::http2::Connection::graceful_shutdown(self);
+    }
+}
+
+#[cfg(feature = "server-auto")]
+impl<'a, I, B, S, E> GracefulConnection for crate::server::conn::auto::Connection<'a, I, S, E>
+where
+    S: hyper::service::Service<http::Request<hyper::body::Incoming>, Response = http::Response<B>>,
+    S::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+    S::Future: 'static,
+    I: hyper::rt::Read + hyper::rt::Write + Unpin + 'static,
+    B: hyper::body::Body + 'static,
+    B::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+    E: hyper::rt::bounds::Http2ServerConnExec<S::Future, B>,
+{
+    type Error = Box<dyn std::error::Error + Send + Sync>;
+
+    fn graceful_shutdown(self: Pin<&mut Self>) {
+        crate::server::conn::auto::Connection::graceful_shutdown(self);
+    }
+}
+
+#[cfg(feature = "server-auto")]
+impl<'a, I, B, S, E> GracefulConnection
+    for crate::server::conn::auto::UpgradeableConnection<'a, I, S, E>
+where
+    S: hyper::service::Service<http::Request<hyper::body::Incoming>, Response = http::Response<B>>,
+    S::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+    S::Future: 'static,
+    I: hyper::rt::Read + hyper::rt::Write + Unpin + Send + 'static,
+    B: hyper::body::Body + 'static,
+    B::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+    E: hyper::rt::bounds::Http2ServerConnExec<S::Future, B>,
+{
+    type Error = Box<dyn std::error::Error + Send + Sync>;
+
+    fn graceful_shutdown(self: Pin<&mut Self>) {
+        crate::server::conn::auto::UpgradeableConnection::graceful_shutdown(self);
+    }
+}
+
+mod private {
+    pub trait Sealed {}
+
+    #[cfg(feature = "http1")]
+    impl<I, B, S> Sealed for hyper::server::conn::http1::Connection<I, S>
+    where
+        S: hyper::service::HttpService<hyper::body::Incoming, ResBody = B>,
+        S::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+        I: hyper::rt::Read + hyper::rt::Write + Unpin + 'static,
+        B: hyper::body::Body + 'static,
+        B::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+    {
+    }
+
+    #[cfg(feature = "http1")]
+    impl<I, B, S> Sealed for hyper::server::conn::http1::UpgradeableConnection<I, S>
+    where
+        S: hyper::service::HttpService<hyper::body::Incoming, ResBody = B>,
+        S::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+        I: hyper::rt::Read + hyper::rt::Write + Unpin + 'static,
+        B: hyper::body::Body + 'static,
+        B::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+    {
+    }
+
+    #[cfg(feature = "http2")]
+    impl<I, B, S, E> Sealed for hyper::server::conn::http2::Connection<I, S, E>
+    where
+        S: hyper::service::HttpService<hyper::body::Incoming, ResBody = B>,
+        S::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+        I: hyper::rt::Read + hyper::rt::Write + Unpin + 'static,
+        B: hyper::body::Body + 'static,
+        B::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+        E: hyper::rt::bounds::Http2ServerConnExec<S::Future, B>,
+    {
+    }
+
+    #[cfg(feature = "server-auto")]
+    impl<'a, I, B, S, E> Sealed for crate::server::conn::auto::Connection<'a, I, S, E>
+    where
+        S: hyper::service::Service<
+            http::Request<hyper::body::Incoming>,
+            Response = http::Response<B>,
+        >,
+        S::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+        S::Future: 'static,
+        I: hyper::rt::Read + hyper::rt::Write + Unpin + 'static,
+        B: hyper::body::Body + 'static,
+        B::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+        E: hyper::rt::bounds::Http2ServerConnExec<S::Future, B>,
+    {
+    }
+
+    #[cfg(feature = "server-auto")]
+    impl<'a, I, B, S, E> Sealed for crate::server::conn::auto::UpgradeableConnection<'a, I, S, E>
+    where
+        S: hyper::service::Service<
+            http::Request<hyper::body::Incoming>,
+            Response = http::Response<B>,
+        >,
+        S::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+        S::Future: 'static,
+        I: hyper::rt::Read + hyper::rt::Write + Unpin + Send + 'static,
+        B: hyper::body::Body + 'static,
+        B::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+        E: hyper::rt::bounds::Http2ServerConnExec<S::Future, B>,
+    {
+    }
+}
+
+#[cfg(test)]
+mod test {
+    // TODO
+}
