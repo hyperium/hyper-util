8e5db0f08cfb6fa08c6ca20c17ef24cfcc83972d
glendc <glen.decauwsemaecker@otainsight.com>
2024-01-13 22:57:09 +0100

    add graceful tests + drop tokio dependency

--- src/server/graceful.rs
+++ src/server/graceful.rs
@@ -6,17 +6,15 @@
 //! See <https://github.com/hyperium/hyper-util/blob/master/examples/server_graceful.rs>
 //! for an example of how to use this.
 
-use futures_util::FutureExt;
 use pin_project_lite::pin_project;
 use slab::Slab;
 use std::{
     fmt::{self, Debug},
     future::Future,
-    pin::{pin, Pin},
+    pin::Pin,
     sync::{atomic::AtomicUsize, Arc, Mutex},
     task::{self, Poll, Waker},
 };
-use tokio::select;
 
 /// A graceful shutdown watcher
 pub struct GracefulShutdown {
@@ -92,10 +90,7 @@ impl Drop for GracefulWatcher {
 impl GracefulWatcher {
     /// Watch a future for graceful shutdown,
     /// returning a wrapper that can be awaited on.
-    pub fn watch<'a, C>(&'a self, conn: C) -> GracefulFuture<C::Error>
-    where
-        C: GracefulConnection + Send + Sync + 'a,
-    {
+    pub fn watch<C: GracefulConnection>(&self, conn: C) -> GracefulFuture<C> {
         // add a counter for this future to ensure it is taken into account
         self.state.subscribe();
 
@@ -107,26 +102,12 @@ impl GracefulWatcher {
             },
         };
 
+        let future = GracefulConnectionFuture::new(conn, cancel);
+
         // return the graceful future, ready to be shutdown,
         // and handling all the hyper graceful logic
         GracefulFuture {
-            future: Box::pin(async move {
-                let mut conn = pin!(conn);
-                let mut cancel = pin!(cancel.fuse());
-
-                loop {
-                    select! {
-                        _ = cancel.as_mut() => {
-                            tracing::trace!("signal received: initiate graceful shutdown");
-                            conn.as_mut().graceful_shutdown();
-                        }
-                        result = conn.as_mut() => {
-                            tracing::trace!("connection finished");
-                            return result;
-                        }
-                    }
-                }
-            }),
+            future,
             state: Some(self.state.clone()),
         }
     }
@@ -171,21 +152,24 @@ pin_project! {
     ///
     /// Whether or not this future panics in such cases is an implementation detail
     /// and should not be relied upon.
-    pub struct GracefulFuture<'a, E> {
+    pub struct GracefulFuture<C> {
         #[pin]
-        future: BoxFuture<'a, E>,
+        future: GracefulConnectionFuture<C, GracefulWaiter>,
         state: Option<Arc<GracefulState>>,
     }
 }
 
-impl<'a, F> Debug for GracefulFuture<'a, F> {
+impl<C> Debug for GracefulFuture<C> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("GracefulFuture").finish()
     }
 }
 
-impl<'a, E> Future for GracefulFuture<'a, E> {
-    type Output = Result<(), E>;
+impl<C> Future for GracefulFuture<C>
+where
+    C: GracefulConnection,
+{
+    type Output = C::Output;
 
     fn poll(self: Pin<&mut Self>, cx: &mut task::Context<'_>) -> Poll<Self::Output> {
         let this = self.project();
@@ -249,7 +233,61 @@ struct GracefulWaiterState {
     key: Option<usize>,
 }
 
-type BoxFuture<'a, E> = Pin<Box<dyn Future<Output = Result<(), E>> + Send + Sync + 'a>>;
+impl Drop for GracefulWaiterState {
+    /// When the waiter is dropped, we need to remove its waker from the waker list.
+    /// As to ensure the graceful waiter is cancel safe.
+    fn drop(&mut self) {
+        if let Some(key) = self.key.take() {
+            let mut wakers = self.state.waker_list.lock().unwrap();
+            wakers.remove(key);
+        }
+    }
+}
+
+pin_project! {
+    struct GracefulConnectionFuture<C, F> {
+        #[pin]
+        conn: C,
+        #[pin]
+        cancel: F,
+        cancelled: bool,
+    }
+}
+
+impl<C, F> GracefulConnectionFuture<C, F> {
+    fn new(conn: C, cancel: F) -> Self {
+        Self {
+            conn,
+            cancel,
+            cancelled: false,
+        }
+    }
+}
+
+impl<C, F> Debug for GracefulConnectionFuture<C, F> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("GracefulConnectionFuture").finish()
+    }
+}
+
+impl<C, F> Future for GracefulConnectionFuture<C, F>
+where
+    C: GracefulConnection,
+    F: Future,
+{
+    type Output = C::Output;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut task::Context<'_>) -> Poll<Self::Output> {
+        let mut this = self.project();
+        if !*this.cancelled {
+            if let Poll::Ready(_) = this.cancel.poll(cx) {
+                *this.cancelled = true;
+                this.conn.as_mut().graceful_shutdown();
+            }
+        }
+        this.conn.poll(cx)
+    }
+}
 
 /// An internal utility trait as an umbrella target for all (hyper) connection
 /// types that the [`GracefulShutdown`] can watch.
@@ -403,5 +441,184 @@ mod private {
 
 #[cfg(test)]
 mod test {
-    // TODO
+    use super::*;
+    use pin_project_lite::pin_project;
+    use std::sync::atomic::{AtomicUsize, Ordering};
+
+    pin_project! {
+        #[derive(Debug)]
+        struct DummyConnection<F> {
+            #[pin]
+            future: F,
+            shutdown_counter: Arc<AtomicUsize>,
+        }
+    }
+
+    impl<F> private::Sealed for DummyConnection<F> {}
+
+    impl<F: Future> GracefulConnection for DummyConnection<F> {
+        type Error = ();
+
+        fn graceful_shutdown(self: Pin<&mut Self>) {
+            self.shutdown_counter.fetch_add(1, Ordering::SeqCst);
+        }
+    }
+
+    impl<F: Future> Future for DummyConnection<F> {
+        type Output = Result<(), ()>;
+
+        fn poll(self: Pin<&mut Self>, cx: &mut task::Context<'_>) -> Poll<Self::Output> {
+            match self.project().future.poll(cx) {
+                Poll::Ready(_) => Poll::Ready(Ok(())),
+                Poll::Pending => Poll::Pending,
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn test_graceful_shutdown_ok() {
+        let graceful = GracefulShutdown::new();
+        let shutdown_counter = Arc::new(AtomicUsize::new(0));
+        let (dummy_tx, _) = tokio::sync::broadcast::channel(1);
+
+        for i in 1..=3 {
+            let watcher = graceful.watcher();
+            let mut dummy_rx = dummy_tx.subscribe();
+            let shutdown_counter = shutdown_counter.clone();
+
+            tokio::spawn(async move {
+                let future = async move {
+                    tokio::time::sleep(std::time::Duration::from_millis(i * 50)).await;
+                    let _ = dummy_rx.recv().await;
+                };
+                let dummy_conn = DummyConnection {
+                    future,
+                    shutdown_counter,
+                };
+                let conn = watcher.watch(dummy_conn);
+                conn.await.unwrap();
+            });
+        }
+
+        assert_eq!(shutdown_counter.load(Ordering::SeqCst), 0);
+        let _ = dummy_tx.send(());
+
+        tokio::select! {
+            _ = tokio::time::sleep(std::time::Duration::from_millis(500)) => {
+                panic!("timeout")
+            },
+            _ = graceful.shutdown() => {
+                assert_eq!(shutdown_counter.load(Ordering::SeqCst), 3);
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn test_graceful_shutdown_delayed_ok() {
+        let graceful = GracefulShutdown::new();
+        let shutdown_counter = Arc::new(AtomicUsize::new(0));
+
+        for i in 1..=3 {
+            let watcher = graceful.watcher();
+            let shutdown_counter = shutdown_counter.clone();
+
+            tokio::spawn(async move {
+                tokio::time::sleep(std::time::Duration::from_millis(i * 5)).await;
+                let future = async move {
+                    tokio::time::sleep(std::time::Duration::from_millis(i * 50)).await;
+                };
+                let dummy_conn = DummyConnection {
+                    future,
+                    shutdown_counter,
+                };
+                let conn = watcher.watch(dummy_conn);
+                conn.await.unwrap();
+            });
+        }
+
+        assert_eq!(shutdown_counter.load(Ordering::SeqCst), 0);
+
+        tokio::select! {
+            _ = tokio::time::sleep(std::time::Duration::from_millis(500)) => {
+                panic!("timeout")
+            },
+            _ = graceful.shutdown() => {
+                assert_eq!(shutdown_counter.load(Ordering::SeqCst), 3);
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn test_graceful_shutdown_multi_per_watcher_ok() {
+        let graceful = GracefulShutdown::new();
+        let shutdown_counter = Arc::new(AtomicUsize::new(0));
+
+        for i in 1..=3 {
+            let watcher = graceful.watcher();
+            let shutdown_counter = shutdown_counter.clone();
+
+            tokio::spawn(async move {
+                let mut futures = Vec::new();
+                for u in 1..=i {
+                    let future = tokio::time::sleep(std::time::Duration::from_millis(u * 50));
+                    let dummy_conn = DummyConnection {
+                        future,
+                        shutdown_counter: shutdown_counter.clone(),
+                    };
+                    let conn = watcher.watch(dummy_conn);
+                    futures.push(conn);
+                }
+                futures_util::future::join_all(futures).await;
+            });
+        }
+
+        assert_eq!(shutdown_counter.load(Ordering::SeqCst), 0);
+
+        tokio::select! {
+            _ = tokio::time::sleep(std::time::Duration::from_millis(500)) => {
+                panic!("timeout")
+            },
+            _ = graceful.shutdown() => {
+                assert_eq!(shutdown_counter.load(Ordering::SeqCst), 6);
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn test_graceful_shutdown_timeout() {
+        let graceful = GracefulShutdown::new();
+        let shutdown_counter = Arc::new(AtomicUsize::new(0));
+
+        for i in 1..=3 {
+            let watcher = graceful.watcher();
+            let shutdown_counter = shutdown_counter.clone();
+
+            tokio::spawn(async move {
+                let future = async move {
+                    if i == 1 {
+                        std::future::pending::<()>().await
+                    } else {
+                        std::future::ready(()).await
+                    }
+                };
+                let dummy_conn = DummyConnection {
+                    future,
+                    shutdown_counter,
+                };
+                let conn = watcher.watch(dummy_conn);
+                conn.await.unwrap();
+            });
+        }
+
+        assert_eq!(shutdown_counter.load(Ordering::SeqCst), 0);
+
+        tokio::select! {
+            _ = tokio::time::sleep(std::time::Duration::from_millis(500)) => {
+                assert_eq!(shutdown_counter.load(Ordering::SeqCst), 3);
+            },
+            _ = graceful.shutdown() => {
+                panic!("shutdown should not be completed: as not all our conns finish")
+            }
+        }
+    }
 }
