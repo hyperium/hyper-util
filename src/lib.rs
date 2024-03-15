7d69601b05d8f87514e8292ecee579650ee50aee
tottoto <tottotodev@gmail.com>
2024-03-16 08:36:24 +0900

    feat(service): relax service module feature

--- src/lib.rs
+++ src/lib.rs
@@ -12,11 +12,7 @@ mod common;
 pub mod rt;
 #[cfg(feature = "server")]
 pub mod server;
-#[cfg(all(
-    feature = "service",
-    any(feature = "http1", feature = "http2"),
-    any(feature = "server", feature = "client")
-))]
+#[cfg(feature = "service")]
 pub mod service;
 
 mod error;
