912dc78c99451fcc0419d49dde130948c5453670
arpad voros <34720962+arpadav@users.noreply.github.com>
2025-04-03 13:22:27 -0400

    fix(client): remove occasional tracing span panic in GaiResolver (#179)

--- src/client/legacy/connect/dns.rs
+++ src/client/legacy/connect/dns.rs
@@ -30,7 +30,6 @@ use std::{fmt, io, vec};
 
 use tokio::task::JoinHandle;
 use tower_service::Service;
-use tracing::debug_span;
 
 pub(super) use self::sealed::Resolve;
 
@@ -117,9 +116,7 @@ impl Service<Name> for GaiResolver {
     }
 
     fn call(&mut self, name: Name) -> Self::Future {
-        let span = debug_span!("resolve", host = %name.host);
         let blocking = tokio::task::spawn_blocking(move || {
-            let _enter = span.enter();
             (&*name.host, 0)
                 .to_socket_addrs()
                 .map(|i| SocketAddrs { iter: i })
