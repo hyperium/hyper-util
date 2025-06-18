7613290a2f02395380450520481454fec09361f2
Sean McArthur <sean@seanmonstar.com>
2025-06-18 08:46:23 -0400

    refactor(client): remove old body delay_eof code

--- src/client/legacy/client.rs
+++ src/client/legacy/client.rs
@@ -358,21 +358,8 @@ where
         // It won't be ready if there is a body to stream.
         if pooled.is_http2() || !pooled.is_pool_enabled() || pooled.is_ready() {
             drop(pooled);
-        } else if !res.body().is_end_stream() {
-            //let (delayed_tx, delayed_rx) = oneshot::channel::<()>();
-            //res.body_mut().delayed_eof(delayed_rx);
-            let on_idle = poll_fn(move |cx| pooled.poll_ready(cx)).map(move |_| {
-                // At this point, `pooled` is dropped, and had a chance
-                // to insert into the pool (if conn was idle)
-                //drop(delayed_tx);
-            });
-
-            self.exec.execute(on_idle);
         } else {
-            // There's no body to delay, but the connection isn't
-            // ready yet. Only re-insert when it's ready
             let on_idle = poll_fn(move |cx| pooled.poll_ready(cx)).map(|_| ());
-
             self.exec.execute(on_idle);
         }
 
