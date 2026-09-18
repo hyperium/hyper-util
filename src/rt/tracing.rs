87887176adaccbdb70893be7f150d97499f09413
katelyn martin <git@katelyn.world>
2026-09-18 00:00:00 +0000

    feat(rt/tracing): introduce `WithSpanExecutor`
    see https://github.com/hyperium/hyper-util/pull/322 for more
    information.
    
    this commit introduces an additional `Executor` implementation to
    accompany the `CurrentSpanExecutor` that executes spawned futures within
    the current span at time of execution.
    
    this would provide an alternative for users that wish to provide tracing
    information, but do not want to run background futures in the current
    span, which can interfere with some observability systems.
    
    Signed-off-by: katelyn martin <git@katelyn.world>

--- src/rt/tracing.rs
+++ src/rt/tracing.rs
@@ -1,5 +1,8 @@
 use hyper::rt::Executor;
-use tracing::instrument::{Instrument, Instrumented};
+use tracing::{
+    Span,
+    instrument::{Instrument, Instrumented},
+};
 
 /// An executor that propagates the current tracing span to its futures.
 ///
@@ -24,6 +27,33 @@ pub struct CurrentSpanExecutor<E> {
     inner: E,
 }
 
+/// An executor that propagates a provided tracing span to its futures.
+///
+/// The span provided to this executor is entered each time the future is
+/// polled or dropped. Execution is delegated to the wrapped executor, without
+/// requiring a particular runtime.
+///
+/// Requires the `tracing` feature.
+///
+/// # Example
+///
+/// ```
+/// # #[cfg(feature = "tokio")]
+/// # {
+/// use hyper_util::rt::{TokioExecutor, WithSpanExecutor};
+///
+/// let span = tracing::info_span!("example");
+/// let executor = WithSpanExecutor::new(TokioExecutor::new(), span);
+/// # }
+/// ```
+#[derive(Clone, Debug)]
+pub struct WithSpanExecutor<E> {
+    inner: E,
+    span: Span,
+}
+
+// ===== impl CurrentSpanExecutor =====
+
 impl<E> CurrentSpanExecutor<E> {
     /// Wrap an executor to propagate the current tracing span to its futures.
     pub fn new(inner: E) -> Self {
@@ -41,9 +71,28 @@ where
     }
 }
 
+// ===== impl WithSpanExecutor =====
+
+impl<E> WithSpanExecutor<E> {
+    /// Wrap an executor to propagate the provided tracing span to its futures.
+    pub fn new(inner: E, span: Span) -> Self {
+        Self { inner, span }
+    }
+}
+
+impl<E, F> Executor<F> for WithSpanExecutor<E>
+where
+    E: Executor<Instrumented<F>>,
+    F: Future,
+{
+    fn execute(&self, future: F) {
+        self.inner.execute(future.instrument(self.span.clone()));
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::CurrentSpanExecutor;
+    use super::{CurrentSpanExecutor, WithSpanExecutor};
     use hyper::rt::Executor;
     use std::{cell::RefCell, future::poll_fn, pin::Pin, task::Poll};
 
@@ -59,7 +108,7 @@ mod tests {
     }
 
     #[test]
-    fn propagates_span_from_execute_on_each_poll() {
+    fn current_span_executor_propagates_span_from_execute_on_each_poll() {
         let _subscriber = tracing::subscriber::set_default(tracing_subscriber::registry());
         let construction_span = tracing::info_span!("construction");
         let execution_span = tracing::info_span!("execution");
@@ -74,6 +123,43 @@ mod tests {
         execution_span.in_scope(|| {
             executor.execute(poll_fn(|_| {
                 assert_eq!(tracing::Span::current().id(), execution_span.id());
+                *polls.borrow_mut() += 1;
+                if *polls.borrow() == 1 {
+                    Poll::Pending
+                } else {
+                    Poll::Ready(())
+                }
+            }));
+        });
+
+        let _entered = polling_span.enter();
+        let mut task = tokio_test::task::spawn(inner.future.borrow_mut().take().unwrap());
+        assert!(task.poll().is_pending());
+        assert_eq!(tracing::Span::current().id(), polling_span.id());
+        assert!(task.poll().is_ready());
+        assert_eq!(tracing::Span::current().id(), polling_span.id());
+        assert_eq!(*polls.borrow(), 2);
+    }
+
+    #[test]
+    fn with_span_executor_propagates_given_span_on_each_poll() {
+        let _subscriber = tracing::subscriber::set_default(tracing_subscriber::registry());
+        let construction_span = tracing::info_span!("construction");
+        let execution_span = tracing::info_span!("execution");
+        let polling_span = tracing::info_span!("polling");
+        let with_span = tracing::info_span!("with");
+        assert!(execution_span.id().is_some());
+
+        // Borrowing a local executor and future also checks that the wrapper
+        // does not impose Send or 'static bounds on the inner executor.
+        let polls = RefCell::new(0);
+        let inner = DeferredExecutor::default();
+        let executor =
+            construction_span.in_scope(|| WithSpanExecutor::new(&inner, with_span.clone()));
+        execution_span.in_scope(|| {
+            executor.execute(poll_fn(|_| {
+                // Execution happens within the given span.
+                assert_eq!(tracing::Span::current().id(), with_span.id());
                 *polls.borrow_mut() += 1;
                 if *polls.borrow() == 1 {
                     Poll::Pending
