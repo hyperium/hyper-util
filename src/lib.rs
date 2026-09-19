181486914f567ce7914153b75ac862c90c5fd462
katelyn martin <git@katelyn.world>
2026-09-19 00:00:00 +0000

    docs: add crate-level documentation
    this commit introduces crate-level documentation outlining the feature
    flags exposed by this library.
    
    in hyperium/hyper-util#322 (and the related hyperium/hyper-util#323), we
    made some changes to the behavior of `TokioExecutor<I>` when the
    `tracing` feature flag is enabled, and introduced a temporary opt-in
    flag to facilitate upgrading when these changes are released.
    
    this should help make this discoverable and friendly for dependents, and
    documentation for the other feature flags is helpful to have as well.
    
    Signed-off-by: katelyn martin <git@katelyn.world>

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,52 @@
 //!
 //! This crate is less-stable than [`hyper`](https://docs.rs/hyper). However,
 //! does respect Rust's semantic version regarding breaking changes.
+//!
+//! # Feature flags
+//!
+//! `hyper-util` relies on feature flags to allow dependents to opt into
+//! building particular components. None of these feature flags are enabled by
+//! default. You may use the `full` feature flag to enable all of the standard
+//! features.
+//!
+//! Note that the `full` feature flag may introduce superfluous dependencies.
+//! You may improve compilation times by enabling the individual features that
+//! you need.
+//!
+//! **Client features**
+//!
+//! * `client`: Enable [`client`] interfaces.
+//! * `client-legacy`: Enable the legacy
+//!   [`Client`][crate::client::legacy::Client] implementation.
+//! * `client-pool`: Enable [`client::pool`]. This submodule contains
+//!   interfaces for constructing connection pools.
+//! * `client-proxy`: Enable [`client::proxy`].
+//! * `client-proxy-system`: Enable platform-specific system proxy support.
+//!
+//! **Server features**
+//!
+//! * `server`: Enable [`server`] interfaces.
+//! * `server-auto`: Enable automatic HTTP version.
+//! * `server-graceful`: Enable [`server::graceful`] interfaces for gracefully
+//!   shutting down a server. See the module-level documentation of
+//!   [`server::graceful`] for more information.
+//!
+//! **Other features**
+//!
+//! * `service`: Enable [`service`] interfaces for compatibility with tower's
+//!   [`Service`][tower_service::Service]. See the module-level documentation
+//!   of [`service`] for more information.
+//! * `http1`: Enable HTTP/1 support.
+//! * `http2`: Enable HTTP/2 support.
+//! * `tokio`: Enable [`rt::tokio`] runtime components to integrate with
+//!   [`tokio`]. See the module-level documentation of [`rt::tokio`] for more
+//!   information.
+//! * `tracing`: Enable [`tracing`] integration.
+//! * `rt-tracing-exec-force`: A temporary compatibility opt-in feature to
+//!   facilitate migrating telemetry from v0.1.20. If set,
+//!   [`rt::tokio::TokioExecutor<I>`] will continue to propagate the current
+//!   [`tracing::Span`] to spawned tasks. This may be removed in a future
+//!   release.
 
 #[cfg(feature = "client")]
 pub mod client;
